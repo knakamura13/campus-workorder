f1ed219ec9125ce6a763f02fbea64e5034f2f591
Kyle Nakamura <knakamura13@apu.edu>
2017-06-22 14:04:33 -0700

    new mac pro

--- controller/export_script.php
+++ controller/export_script.php
@@ -5,16 +5,14 @@ error_reporting(0);
 	    							  $_POST['lastNameField'] . "\n" .
 	    	 	"Phone: \t\t"  		. $_POST['phoneField']   . "\n" .
 	    	 	"Email: \t\t" 		. $_POST['emailField']   . "\n" .
-	    	 	"Campus: \t" 		. $_POST['campusSection']   . "\n" .
-	    	 	"Building: \t" 		. $_POST['buildingSection']  . "\n" .
-	    		"Department: \t" 	. $_POST['departmentSection']   . "\n" .
-	    		"Service: \t"		. $_POST['serviceOne'] . $_POST['serviceTwo'] . 
-	    							  $_POST['serviceThree'] . $_POST['serviceFour'] . 
-	    							  $_POST['serviceFive'] . $_POST['serviceSix'] .  "\n" .
-	    		"Details: \t" 		. trim(preg_replace('/\s\s+/', ' ', $_POST['informationSection']))  . "\n" .
-	    		"Image: \t" 		. $_POST['imageSection']  . "\n" .
+	    	 	"Campus: \t" 		. $_POST['campusRadio']   . "\n" .
+	    	 	"Building: \t" 		. $_POST['buildingDropdown']  . "\n" .
+	    		"Department: \t" 	. $_POST['departmentDropdown']   . "\n" .
+	    		"Details: \t" 		. trim(preg_replace('/\s\s+/', ' ', $_POST['informationText']))  . "\n" .
+	    		"Image: \t" 		. $_POST['imageFile']  . "\n" .
 	    		"-------------------------------------------------------------- \n";
-	    $ret = file_put_contents('../view/form_data.txt', $data, FILE_APPEND | LOCK_EX);
+	    $ret = file_put_contents('../view/formData.txt', $data, FILE_APPEND);
+	    chmod("../view/formData.txt", 0777);
 	    if($ret === false) {
 	        die('There was an error writing this file');
 	    }
