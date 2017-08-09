4e2bd708e346f74962f4f756362e2486d27004ef
Kyle Nakamura <knakamura13@apu.edu>
2017-08-09 09:48:35 -0700

    finish setting up ticket creation, need to merge with psmbl now

--- vendor/zendesk/zendesk_api_client_php/samples/tickets/createTicketWithAttachment.php
+++ vendor/zendesk/zendesk_api_client_php/samples/tickets/createTicketWithAttachment.php
@@ -8,25 +8,53 @@ use Zendesk\API\HttpClient as ZendeskAPI;
 // Post Variables
 $description = $_POST['description'];
 $location = $_POST['location'];
-// $pride = $_POST['pride'];
-// $email = $_POST['email'];
-// $phone = $_POST['phone'];
 $image = $_FILES['image'];
+$pride = $_POST['pride'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
 
 $subdomain = "apu1502217909";
 $username  = "knakamura13@apu.edu";
 $token     = "hVTTqdotz8hLoAkuT5LFBNtG9iu6tanymZZ51gGX";
 
-
 $targetdir = '../../../../../uploads/';
 $targetfile = $targetdir.$_FILES['image']['name'];
 $attachment = $targetfile;
 
+// Build message body
+$textBody = "";
+if (isset($_POST["description"])) {
+    $textBody .= $description."\n";
+    $textBody = preg_replace('/^[ \t]*[\r\n]+/m', '', $textBody);
+    $textBody .= "---END MESSAGE---\n\n";
+}
+if (isset($_POST["location"])) {
+    $textBody .= "Location: ";
+    $textBody .= $location."\n";
+}
+if (isset($_POST["pride"])) {
+    $textBody .= "Pride of place: ";
+    if ($pride === "on") {
+        $textBody .= "YES\n";
+    } else {
+        $textBody .= "NO\n";
+    }
+}
+if (isset($_POST["phone"]) || (isset($_POST["email"]))) {
+    $textBody .= "CONTACT INFORMATION:\n";
+    if (isset($_POST["phone"])) {
+        $textBody .= "Phone: ";
+        $textBody .= $phone."\n";
+    }
+    if (isset($_POST["email"])) {
+        $textBody .= "Email: ";
+        $textBody .= $email."\n";
+    }
+}
+
+$client = new ZendeskAPI($subdomain);
+$client->setAuth('basic', ['username' => $username, 'token' => $token]);
 if (move_uploaded_file($_FILES['image']['tmp_name'], $targetfile)) {
-
-    $client = new ZendeskAPI($subdomain);
-    $client->setAuth('basic', ['username' => $username, 'token' => $token]);
-
     try {
         // Upload file
         $attachment = $client->attachments()->upload([
@@ -34,39 +62,7 @@ if (move_uploaded_file($_FILES['image']['tmp_name'], $targetfile)) {
             'type' => 'image/png',
             'name' => $_FILES['image']['name']
         ]);
-
-
-        // Build message body
-        $textBody = "";
-        if (isset($_POST["description"])) {
-            $textBody .= "<message>\n";
-            $textBody .= $description."\n";
-            $textBody = preg_replace('/^[ \t]*[\r\n]+/m', '', $textBody);
-            $textBody .= "</message>\n\n";
-        }
-        if (isset($_POST["location"])) {
-            $textBody .= "Location: \t";
-            $textBody .= $location."\n";
-        }
-        if (isset($_POST["pride"])) {
-            $textBody .= "Pride of place: \t";
-            if ($pride === "on") {
-                $textBody .= "YES\n";
-            } else {
-                $textBody .= "NO\n";
-            }
-        }
-        $textBody .= "Contact information:\n";
-        if (isset($_POST["phone"])) {
-            $textBody .= "Phone: \t";
-            $textBody .= $phone."\n";
-        }
-        if (isset($_POST["email"])) {
-            $textBody .= "Email: \t";
-            $textBody .= $email."\n";
-        }
-
-
+        
         // Create a new ticket with attachment
         $newTicket = $client->tickets()->create([
             'type' => 'problem',
@@ -75,28 +71,41 @@ if (move_uploaded_file($_FILES['image']['tmp_name'], $targetfile)) {
                 'body' => $textBody,
                 'uploads' => [$attachment->upload->token]
             ),
-            'requester' => array(
-                'locale_id' => '1',
-                'name' => 'Kyle Nakamura',
-                'email' => $username,
-            ),
+            // 'requester' => array(
+            //     'locale_id' => '1',
+            //     'name' => 'Kyle Nakamura',
+            //     'email' => $username,
+            // ),
             'priority' => 'normal',
         ]);
-
-        $ticketID = $newTicket->ticket->id;
-        if (is_numeric($ticketID)) {
-        echo "The Zendesk ticket was created.";
-        echo "<script>";
-            $url = 'https://'.$subdomain.'.zendesk.com/agent/tickets/'.$ticketID;
-            echo "window.open('$url');".PHP_EOL;
-            echo "history.go(-1);";
-        echo "</script>";
-        } else {
-            echo "There was an error and the Zendesk ticket was not created.";
-        }
     } catch (\Zendesk\API\Exceptions\ApiResponseException $e) {
         echo $e->getMessage().'</br>';
     }
 } else {
+    // Create a new ticket without attachment
+    $newTicket = $client->tickets()->create([
+        'type' => 'problem',
+        'subject'  => 'The quick brown fox',
+        'comment'  => array(
+            'body' => $textBody
+        ),
+        // 'requester' => array(
+        //     'locale_id' => '1',
+        //     'name' => 'Kyle Nakamura',
+        //     'email' => $username,
+        // ),
+        'priority' => 'normal',
+    ]);
+}
 
+$ticketID = $newTicket->ticket->id;
+if (is_numeric($ticketID)) {
+echo "The Zendesk ticket was created.";
+echo "<script>";
+    $url = 'https://'.$subdomain.'.zendesk.com/agent/tickets/'.$ticketID;
+    echo "window.open('$url');".PHP_EOL;
+    echo "history.go(-1);";
+echo "</script>";
+} else {
+    echo "There was an error and the Zendesk ticket was not created.";
 }
